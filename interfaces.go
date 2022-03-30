a7878b2224eca1256ae5713ab5b7f6d2e4417d3e
AlbenaveGitHub <55168140+AlbenaveGitHub@users.noreply.github.com>
2022-03-30 10:35:20 -0600

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -2,12 +2,12 @@ package figuras
 
 import "fmt"
 
-type Figura interface {
+type figura interface {
 	area() float64
 	perimetro() float64
 }
 
-func Calculo(f Figura) {
+func Calculo(f figura) {
 	fmt.Println("El Calculo es: ", f)
 	fmt.Println("El Area es: ", f.area())
 	fmt.Println("El Perimetro es: ", f.perimetro())
